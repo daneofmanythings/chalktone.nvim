6c39d6fb43b8532d5e71d19cc7ec2f444f32042b
dane <dhcjensen@gmail.com>
2024-03-07 23:00:07 -0800

    Maybe itll be better to look at.

--- lua/firelight/theme.lua
+++ lua/firelight/theme.lua
@@ -11,6 +11,11 @@ M.setup = function(opts)
 		Normal = { fg = c.ui.fg, bg = c.ui.bg },
 		NormalFloat = "Normal",
 
+		Pmenu = "NormalFloat",
+		PmenuSel = { bg = c.ui.fg_highlight },
+		PmenuSbar = "Pmenu",
+		PmenuThumb = "PmenuSel",
+
 		Cursor = { fg = c.ui.bg, bg = c.ui.fg },
 		-- lCursor = {}
 		CursorIM = "Cursor",
