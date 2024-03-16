d551417d86a725f5c616ab295a7964b7f60428a8
dane <dhcjensen@gmail.com>
2024-03-16 12:08:52 -0700

    added nougat.nvim integration.

---
+++ lua/nougat/color/chalktone.lua
@@ -0,0 +1,39 @@
+local M = {}
+
+M.get = function()
+	return {
+		red = '#bd8183' ,
+		green = '#89b3b6',
+		yellow = '#f1b47e',
+		blue = '#9db2d2',
+		magenta = '#cd88b8',
+		cyan = '#89b3b6',
+
+		accent = {
+			red = '#d47766',
+			green = '#9fc6b8',
+			yellow = '#fbdc98',
+			blue = '#cfbfe3',
+			magenta = '#dfaad2',
+			cyan = '#8fd1b9',
+			bg = '#524f4c',
+			fg = '#ece1d7',
+		},
+
+		bg = '#292522',
+		bg0 = '#34302c',
+		bg1 = '#403d3b',
+		bg2 = '#292522',
+		bg3 = '#292522',
+		bg4 = '#292522',
+
+		fg = '#ece1d7',
+		fg0 = '#ece1d7',
+		fg1 = '#ece1d7',
+		fg2 = '#ece1d7',
+		fg3 = '#ece1d7',
+		fg4 = '#ece1d7',
+	}
+end
+
+return M
